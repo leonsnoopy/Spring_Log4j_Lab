14d45025fd9a79606421e19abb4cdc0d5068667f
leon_lin <leon_lin@gss.com.tw>
2024-10-17 17:12:05 +0800

    add

--- src/main/java/com/gss/demo/repository/userRepository.java
+++ src/main/java/com/gss/demo/repository/userRepository.java
@@ -1,8 +1,24 @@
 package com.gss.demo.repository;
 
-import com.gss.demo.enity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 
-public interface userRepository extends JpaRepository<User, Long> {
+@Repository
+public class userRepository {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public List<Object[]> findUserByIdUnsafe(String id) {
+        String sql = "SELECT * FROM users WHERE id = '" + id + "'";
+        Query query = entityManager.createNativeQuery(sql);
+        return query.getResultList();
+    }
 }
